56b6f6b26c84b8af333ff2cc67d3bb2f7bed508a
Noah Fichter <nfichter@stuy.edu>
2016-11-01 12:42:59 -0400

    renamed db

--- story_manager.py
+++ story_manager.py
@@ -1,2 +1,14 @@
-def create_story(userid,title,timestamp,text):
-    
+class Story:
+    story_id = 0
+    title = ""
+    latest_update = ""
+    timestamp_latest_update = 0
+    timestamp_created = 0
+    contributed_to_by_user_ids = ""
+
+    def create(title, text, timestamp, creator_id):
+        self.title = title
+        self.latest_update = text
+        self.timestamp_latest_update = timestamp
+        self.timestamp_created = timestamp
+        self.contributed_to_by_user_ids = ""+creator_id
