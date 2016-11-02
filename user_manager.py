967c3fa3cabf94c1bb02d45bb7348ff0a74136d4
Jessica <jtitensky@stuy.edu>
2016-11-02 00:10:14 +0000

    user functions

--- user_manager.py
+++ user_manager.py
@@ -17,7 +17,7 @@ class User:
         #self.password=hash(unhashed)
 
     def update():
-        c.execute("UPDATE users SET username=%s,password=%s,age=%d,email=%s posts_contributed_to=%s WHERE user_id=%d"%(username,hash(password),age,email,pots_contributed_to,user_id))
+        c.execute("update users set username=%s,password=%s,age=%d,email=%s posts_contributed_to=%s where user_id=%d"%(username,hash(password),age,email,pots_contributed_to,user_id))
         
 def hash(unhashed):
     mho=hashlib.sha1()
@@ -33,20 +33,43 @@ def register(username,password,age,email):
         return 0
     c.execute("select user_id from users")
     data=c.fetchall()
-    c.execute("insert into users values ('%d','%s','%s','%d','%s','');"%(len(data)+1,username,hash(password),age,email))
+    c.execute("insert into users values ('%d','%s','%s','%d','%s','')"%(len(data)+1,username,hash(password),age,email))
     return 1
 
-"""
 #0 no such username
+#1 success
+#2 wrong password
 def login(username,password):
     c.execute("select password from users where username='%s'"%(username))
     data=c.fetchall()
     if len(data)==0:
         return 0
-    print data
+    if data[0][0]==hash(password):
+        return 1
+    return 2
     
-        
-login('homer','simpson')
+#0 no such username
+#1 success
+def remove(username):
+    c.execute("select user_id from users where username='%s'"%(username))
+    data=c.fetchall()
+    if len(data)==0:
+        return 0
+    c.execute("delete from users where username='%s'"%(username))
+    return 1
+
+def exists(username):
+    c.execute("select user_id from users where username='%s'"%(username))
+    data=c.fetchall()
+    return len(data)!=0
+
+"""        
+print register("homer","simpson",20,"blah@gmail.com")
+print login('homer','simpson')
+print login("homer","as")
+print exists("homer")
+print remove("homer")
+print exists("homer")
 """
 
 db.commit()
