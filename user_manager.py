7ea4f126975fc4427a9b124e3d500fbbcb0f697c
Jessica Titensky <jtitensky@stuy.edu>
2016-11-03 15:43:03 -0400

    update

--- user_manager.py
+++ user_manager.py
@@ -6,6 +6,8 @@ class User:
     user_id=0
     username=""
     password=""
+    first=""
+    last=""
     age=0
     email=""
     posts_contributed_to=""
@@ -17,7 +19,7 @@ class User:
         f="sturdy-octo-train.db"
         db=sqlite3.connect(f)
         c=db.cursor()
-        c.execute("update users set username=%s,password=%s,age=%d,email=%s posts_contributed_to=%s where user_id=%d"%(username,hash(password),age,email,pots_contributed_to,user_id))
+        c.execute("update users set username='%s',password='%s',first='%s',last='%s',age=%d,email='%s', posts_contributed_to='%s' where user_id=%d"%(self.username,hash(self.password),self.first,self.last,self.age,self.email,self.posts_contributed_to,self.user_id))
         db.commit()
         db.close()
                
@@ -26,26 +28,28 @@ def hash(unhashed):
     mho.update(unhashed)
     return mho.hexdigest()
 
-def get(uid):
+def get(un):
     f="sturdy-octo-train.db"
     db=sqlite3.connect(f)
     c=db.cursor()
-    c.execute("select * from users where user_id='%s'"%(uid))
+    c.execute("select * from users where username='%s'"%(un))
     data=c.fetchall()
     user=User()
-    user.user_id=uid
-    user.username=data[0][1]
+    user.username=un
+    user.user_id=data[0][0]
     user.password=data[0][2]
-    user.age=data[0][3]
-    user.email=data[0][4]
-    user.posts_contributed_to=data[0][5]
+    user.first=data[0][3]
+    user.last=data[0][4]
+    user.age=data[0][5]
+    user.email=data[0][6]
+    user.posts_contributed_to=data[0][7]
     db.commit()
     db.close()
     return user
 
 #0 username exists
 #1 success
-def register(username,password,age,email):
+def register(username,password,first,last,age,email):
     f="sturdy-octo-train.db"
     db=sqlite3.connect(f)
     c=db.cursor()
@@ -57,7 +61,7 @@ def register(username,password,age,email):
         return 0
     c.execute("select user_id from users")
     data=c.fetchall()
-    c.execute("insert into users values ('%d','%s','%s','%d','%s','')"%(len(data)+1,username,hash(password),age,email))
+    c.execute("insert into users values ('%d','%s','%s','%s','%s','%d','%s','')"%(len(data)+1,username,hash(password),first,last,age,email))
     db.commit()
     db.close()
     return 1
@@ -117,5 +121,5 @@ print login("homer","as")
 print exists("homer")
 print remove("homer")
 print exists("homer")
-"""
 print get(1)
+"""
